5bc55538e0884e58da1686fd64ce2d79a3830247
Ashraful610 <ashalam610@gmail.com>
2023-04-11 00:43:35 +0600

    querySelectorAll

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-// console.log('helo from main js')
+const sections = document.querySelectorAll('section')
+for(const section of sections){
+   section.style.border = '2px solid tomato'
+   section.style.background='steelblue'
+   section.style.borderRadius = '5px'
+   section.style.margin = '10px'
+   section.style.padding = '10px'
+}
+const motivations = document.querySelectorAll('li')
+for(const motivate of motivations){
+    motivate.style.color = 'white'
+}
